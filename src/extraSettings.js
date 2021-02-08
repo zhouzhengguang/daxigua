a6775f89651ca7338b92a8d41b1b32bebcb257b4
zhouzhengguang <myxing1226@163.com>
2021-02-08 13:12:53 +0800

    bixin

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -3,7 +3,7 @@
  */
 
 // 额外分数：修改数字
-let extraScore = 1;
+let extraScore = 10;
 
 // 无敌模式：true 改为 false
 let wuDi = false;
@@ -26,7 +26,7 @@ let setFruits = {
 }
 
 // 让水果更 Q 弹：false 改为大于 0 小于 1 的任意小数（推荐 0.9）
-let fruitQTan = 0.9;
+let fruitQTan = 0.4;
 
 // 让水果下落缓慢：false 改为大于 0 的任意数，值越大阻力越大，下落越慢（推荐 5）
 let fruitSlowDown = false;
